0153b45a0e51667a476725c9e1a5c5caca877c45
KlugSolutions <tomasz.kluge@gmail.com>
2022-09-03 12:45:38 +0200

    added counter functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,22 @@
-// Variables
+// Variables for open and close popup
 const popup = document.getElementById("myPopup");   // Get element 'Popup'
-const btn = document.getElementById("myBtn");   // Get element 'button'
+const myBtn = document.getElementById("myBtn");   // Get element 'button'
 const img = document.getElementsByClassName("close")[0];    // Get element 'close'
 
+// Variables for counting
+const count = document.getElementById('count'); //Get element for counting
+let counter = 0; // default value of counting (initial value)
+
 // Functions
-btn.onclick = function() {      // Open popup function
-  popup.style.display = "block";
+myBtn.onclick = function () {      // Open popup function
+    popup.style.display = "block"; // show popup
+    counter++; // each click +1
+    count.innerHTML = counter; // return value into html
 }
 
-img.onclick = function() {      // Close popup function
-  popup.style.display = "none";
+img.onclick = function () {      // Close popup function
+    popup.style.display = "none"; // hidden popup
 }
+
+
+
